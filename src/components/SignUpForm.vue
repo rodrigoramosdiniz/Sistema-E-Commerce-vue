59adcd4a5fa4d955afbd136064fe12b1a14901f6
Gustavo Y. Maruyama <gustavo.maruyama@ifms.edu.br>
2019-10-22 21:47:59 -0400

    Signup feito

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -2,12 +2,22 @@
     <b-card title="Sign Up"
             sub-title="Cadastre-se para acessar o sistema">
         <b-form>
+            <b-form-group label="Nome:">
+                <b-form-input
+                        type="text"
+                        required
+                        v-model="user.name"
+                        placeholder="Insira seu nome">
+                </b-form-input>
+            </b-form-group>
+
             <b-form-group
                     label="E-mail:"
                     description="Informe um e-mail válido">
                 <b-form-input
                         type="email"
                         required
+                        v-model="user.email"
                         placeholder="Insira seu e-mail">
                 </b-form-input>
             </b-form-group>
@@ -18,25 +28,36 @@
                 <b-form-input
                         type="password"
                         required
+                        v-model="user.password"
                         placeholder="Insira sua senha">
                 </b-form-input>
             </b-form-group>
         </b-form>
-        <b-button variant="primary">Login</b-button>
+        <b-button v-on:click="emitirEventoParaOPai" variant="primary">Cadastrar</b-button>
     </b-card>
 </template>
 
 <script>
     import {BCard, BForm, BFormGroup, BFormInput, BButton} from 'bootstrap-vue'
-
+    import User from '../models/User'
     export default {
         name: "SignUp",
+        data(){
+            return{
+                user : new User()
+            }
+        },
         components: {
             BCard,
             BForm,
             BFormGroup,
             BFormInput,
             BButton
+        },
+        methods:{
+            emitirEventoParaOPai(){
+                this.$emit('novoUsuario',this.user)
+            }
         }
     }
 </script>
